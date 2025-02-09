1938b1bd1cdff93b4f651b32a1274eb8dc76378f
redrum <franco.avalos@live.com>
2025-02-09 18:56:07 -0300

    feat: new component projects grid

--- Portfolio-2024/astro.config.mjs
+++ Portfolio-2024/astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server', // Cambiar de 'static' a 'server'
+  // Cambiar de 'static' a 'server'
+  output: 'server',
+
+  integrations: [react()]
 });
---
+++ Portfolio-2024/src/components/react/Projects.grid.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react"
+import type { IProject } from "../../interfaces/IProject"
+
+import "./Projects.grid.style.css"
+
+const ProjectsGrid = (props: { projects: Array<Array<IProject>>}) => {
+    const { projects } = props
+
+    const totalPagesProjects = projects?.length - 1;
+
+    const [projectsGrid, setProjectsGrid] = useState<Array<Array<IProject>>>([]);
+    const [projectsGrigPage, setProjectsGrigPage] = useState<number>(0)
+
+    const [showMore, setShowMore] = useState<boolean>(false)
+    const [showLess, setShowLess] = useState<boolean>(false)
+
+    const handleMoreProjects = () => {
+        const currentPage = projectsGrigPage + 1;
+        
+        setProjectsGrid((prev) => {
+            return [...prev, ...[projects[currentPage]]]
+        })
+        setProjectsGrigPage(currentPage)
+        setShowMore(currentPage < totalPagesProjects)
+        setShowLess(true)
+    }
+    const handleLessProjects = () => {
+        setProjectsGrid((prev) => {
+            return prev.slice(0, prev.length - 1)
+        })
+        setProjectsGrigPage(projectsGrigPage - 1)
+        setShowMore(true)
+        setShowLess(projectsGrigPage -1 > 0)
+    }
+
+    useEffect(() => {
+        setProjectsGrid([projects[projectsGrigPage]])
+        setShowMore(totalPagesProjects > 0)
+    }, [projects])
+
+    return (
+        <>
+            <div className="project-grid__container">
+                {
+                    projectsGrid &&
+                    projectsGrid?.map(projects => (
+
+                        projects?.map((project, index) => (
+                            <div className="project-card" key={index}>
+                                <div className="project-card__header">
+                                    <h4>{project.title}</h4>
+                                </div>
+                                <div className="project-card__content">
+                                    <img src="/imgs/repofront.webp" alt="Carpeta" />
+                                </div>
+                                <div className="project-card__base">
+                                    {project && project?.tags.map((tag, index) => (
+                                        <span>{tag}</span>
+                                    ))}
+                                </div>
+                                <div className="project-card__footer">
+                                    <a href={project?.url} target="_blank">
+                                        <img src="/imgs/icons8-github-48.png" alt="Github" />
+                                    </a>
+                                </div>
+                            </div>
+                        ))
+                    ))
+                }
+            </div>
+            <div className="project-grid__more">
+                <div>{showLess && <p onClick={handleLessProjects}>Menos...</p>}</div>
+                <div>{showMore && <p onClick={handleMoreProjects}>Más...</p>}</div>
+            </div>
+        </>
+
+    )
+}
+
+export default ProjectsGrid
