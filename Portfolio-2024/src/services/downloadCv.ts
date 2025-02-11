ff674eab92a6d582dd8fc2e8911476702b1d061a
redrum <franco.avalos@live.com>
2025-02-11 15:17:25 -0300

    feat: responsive nav

--- Portfolio-2024/src/services/downloadCv.ts
+++ Portfolio-2024/src/services/downloadCv.ts
@@ -1,3 +1,15 @@
+import { CV_DICTIONARY } from "../config"
+
 export const downloadCvService = () => {
-    console.log('download from service')
+
+    const link = document.createElement("a");
+
+    link.href = CV_DICTIONARY.ROUTE;
+    link.download = CV_DICTIONARY.FILE;
+    
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    return Promise.resolve();
 }
