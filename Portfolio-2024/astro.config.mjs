bfa01762066641c2e238c85e46952c340bf2e094
redrum <franco.avalos@live.com>
2025-02-26 16:18:28 -0300

    feat: add node server and config vercel app

--- Portfolio-2024/astro.config.mjs
+++ Portfolio-2024/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import dotenv from 'dotenv';
 import path from 'path';
+import vercel from '@astrojs/vercel/serverless'
+import node from '@astrojs/node'
 
 const ENV = process.env.NODE_ENV || 'development'
 
@@ -10,14 +12,9 @@ dotenv.config({
 })
 
 export default defineConfig({
-  env: {
-    API_URL: process.env.API_URL,
-    GITHUB_SITE_URL: process.env.GITHUB_SITE_URL,
-    LINKEDIN_SITE_URL: process.env.LINKEDIN_SITE_URL,
-    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
-    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
-    EMAILJS_PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY,
-  },
+  adapter: vercel(),
+  // adapter: vercel({ entrypoint: "server" }),
+  // adapter: node({ mode: "standalone" }),
   output: 'server',
   integrations: [react()]
 });
